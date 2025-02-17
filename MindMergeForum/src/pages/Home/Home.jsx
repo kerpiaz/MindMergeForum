1663de709cdf694c8fea3fba4426b068f4791896
Radoslav Kalushkov <radi_kalu6kov@abv.bg>
2025-02-17 15:41:46 +0200

    Feature: Home screen now shows both registered users and amount of posts on the site + fixed the head tag in the html to display the name of our forum

--- MindMergeForum/src/pages/Home/Home.jsx
+++ MindMergeForum/src/pages/Home/Home.jsx
@@ -1,21 +1,26 @@
 import { useState, useEffect } from "react";
 import { getTotalUsers } from "../../../services/user.services";
+import { getPosts } from "../../../services/posts.services";
 
 export default function Home() {
   const [totalUsers, setTotalUsers] = useState(0);
+  const [totalPosts, setTotalPosts] = useState(0);
 
   useEffect(() => {
-    async function fetchTotalUsers() {
-      const total = await getTotalUsers();
-      setTotalUsers(total);
+    async function fetchTotalUsersAndPosts() {
+      const totalUsers = await getTotalUsers();
+      setTotalUsers(totalUsers);
+      const posts = await getPosts()
+      const totalPosts = Object.keys(posts).length;
+      setTotalPosts(totalPosts);
     }
-    fetchTotalUsers();
+    fetchTotalUsersAndPosts();
   }, []);
 
   return (
     <div>
       <h2>Welcome to MindMerge Forum! </h2>
-      <p>Register and join our {totalUsers} registered users and start sharing your experience with the world! </p>
+      <p>Register and join our {totalUsers} registered users with more than {totalPosts} posts waiting for you and start sharing your experience with the world!</p>
     </div>
   );
 }
